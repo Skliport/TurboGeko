d9c49a7590b182814e75fa9d353ac1da5a992f24
Julio Canizalez <juliocanizalez@outlook.com>
2019-11-04 17:44:43 -0600

    query receta

--- src/DB/mod_vt.sql
+++ src/DB/mod_vt.sql
@@ -42,7 +42,32 @@ CREATE TABLE sale_detail(
 	id_sale int NOT NULL,
 	product_id int NOT NULL
 );
+--TABLA DE RECETA
+CREATE TABLE recipe (id_recipe SERIAL PRIMARY KEY, 
+cod_recipe varchar(20), 
+unit_cost REAL, sale_price REAL, raw_income REAL NULL,
+id_product INT);
 
+CREATE TABLE supply_for_recipe(id_supply SERIAL PRIMARY KEY, 
+quantity REAL, unit_cost REAL, 
+total_cost REAL,id_measurement int, id_recipe int
+id_material int);
+
+CREATE TABLE measurement_description(id_measurement SERIAL PRIMARY KEY,
+description varchar(60));
+
+ALTER TABLE supply_for_recipe ADD CONSTRAINT fk_supply_recipe FOREIGN KEY (id_recipe)
+REFERENCES recipe (id_recipe);
+
+ALTER TABLE supply_for_recipe ADD CONSTRAINT fk_supply_measurement FOREIGN KEY (id_measurement)
+REFERENCES measurement_description (id_measurement);
+
+ALTER TABLE supply_for_recipe ADD CONSTRAINT fk_supply_material FOREIGN KEY (id_material)
+REFERENCES material (id_material);
+
+ALTER TABLE recipe ADD CONSTRAINT fk_recipe_product FOREIGN KEY(id_product)
+REFERENCES product (id_product);
+--FIN DE TABLA RECETA
 ALTER TABLE ONLY customer
 ADD CONSTRAINT pk_customer_id PRIMARY KEY (customer_id);
 
