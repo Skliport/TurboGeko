91e8869af7dac0f5ec2615756399946237eb8e45
RobertooDB <57153706+RobertooDB@users.noreply.github.com>
2019-11-04 18:55:36 -0600

    Update Compra de Materiales.sql

--- src/DB/CI/Compra de Materiales.sql
+++ src/DB/CI/Compra de Materiales.sql
@@ -1,101 +1,186 @@
-CREATE TABLE IF NOT EXISTS employees(
-	employee_id serial PRIMARY KEY
+CREATE TABLE suppliers(
+	supplier_id SERIAL PRIMARY KEY,
+	company_name VARCHAR(60) NOT NULL,
+	contact_name VARCHAR(60) NOT NULL,
+	contact_title VARCHAR(60) NOT NULL,
+	address VARCHAR(70) NOT NULL,
+	email VARCHAR(30) NOT NULL,
+	city VARCHAR(25) NOT NULL,
+	region VARCHAR(20) NOT NULL,
+	postal_code VARCHAR(15) NOT NULL,
+	country VARCHAR(15) NOT NULL,
+	phone VARCHAR(15) NOT NULL,
+	fax VARCHAR(25) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS purchase(
-	purchase_id serial PRIMARY KEY,
-	purchase_date DATE,
-	observations text,
-	material_id smallint,
-	employee_id smallint,
-	subtotal real,
-	total real
+CREATE TABLE materials(
+	material_id SERIAL PRIMARY KEY,
+	supplier_id INT NOT NULL,
+	material_name VARCHAR(25) NOT NULL,
+	units_in_stock INT NOT NULL,
+	material_measure VARCHAR(25) NOT NULL,
+	unit_price REAL NOT NULL,
+	number_lot INT NOT NULL,
+	expiration_date DATE NOT NULL,
+	discontinued INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS purchase_details(
-	purchase_id smallint,
-	material_id smallint,
-	observations text,
-	discount real,
-	unit_price real,
-	discontinued integer
+CREATE TABLE purchase(
+	purchase_id SERIAL PRIMARY KEY,
+	employee_id INT NOT NULL,
+	purchase_date DATE NOT NULL,
+	purchase_status INT NOT NULL,
+	observations TEXT NOT NULL,
+	delay REAL NOT NULL,
+	installment INT NOT NULL,
+	days INT NOT NULL,
+	subtotal REAL NOT NULL,
+	iva REAL NOT NULL,
+	total REAL NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS materials(
-	material_id serial PRIMARY KEY,
-	material_code smallint,
-	material_name character varying(40),
-	quantity_per_unit character varying(20),
-	type character varying(15),
-	unit_price real,
-	date_discontinued date,
-	company_name character varying(40),
-	address_company character varying(60),
-	discontinued integer
+CREATE TABLE purchase_details(
+	purchase_id INT NOT NULL,
+	material_id INT NOT NULL,
+	quantity INT NOT NULL,
+	unit_price REAL NOT NULL
 );
 
-
-CREATE TABLE IF NOT EXISTS materials_old_date(
-	material_id_old smallint,
-	material_code smallint,
-	material_name character varying(20),
-	quantity_per_unit character varying(20),
-	type character varying(15),
-	unit_price real,
-	date_discontinued date,
-	modification_employee smallint,
-	modification_date date
+CREATE TABLE employees(
+	employee_id SERIAL PRIMARY KEY,
+	name VARCHAR(20)
 );
 
-CREATE TABLE IF NOT EXISTS purchase_details_old_date(
-	purchase_id_old smallint,
-	material_id smallint,
-	observations text,
-	discount real,
-	unit_price real,
-	discontinued integer,
-	modification_employee smallint,
-	modification date
+CREATE TABLE suppliers_log(
+	supplier_id INT NOT NULL,
+	company_name VARCHAR(60) NOT NULL,
+	contact_name VARCHAR(60) NOT NULL,
+	contact_title VARCHAR(60) NOT NULL,
+	address VARCHAR(70) NOT NULL,
+	email VARCHAR(30) NOT NULL,
+	city VARCHAR(25) NOT NULL,
+	region VARCHAR(20) NOT NULL,
+	postal_code VARCHAR(15) NOT NULL,
+	country VARCHAR(15) NOT NULL,
+	phone VARCHAR(15) NOT NULL,
+	fax VARCHAR(25) NOT NULL,
+	modification_date_hour TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS purchase_old_date(
-	purchase_id_old_date smallint,
-	purchase_date date,
-	observations text,
-	material_id smallint,
-	employee_id smallint,
-	subtotal real,
-	total real,
-	modification_employee smallint,
-	modification_date date
+CREATE TABLE materials_log(
+	material_id INT NOT NULL,
+	supplier_id INT NOT NULL,
+	material_name VARCHAR(25) NOT NULL,
+	units_in_stock INT NOT NULL,
+	material_measure VARCHAR(25) NOT NULL,
+	unit_price REAL NOT NULL,
+	number_lot INT NOT NULL,
+	expiration_date DATE NOT NULL,
+	discontinued INT NOT NULL,
+	modification_date_hour TIMESTAMP NOT NULL
 );
+
+CREATE TABLE purchase_log(
+	purchase_id INT NOT NULL,
+	employee_id INT NOT NULL,
+	purchase_date DATE NOT NULL,
+	purchase_status INT NOT NULL,
+	observations TEXT NOT NULL,
+	delay REAL NOT NULL,
+	installment INT NOT NULL,
+	days INT NOT NULL,
+	subtotal REAL NOT NULL,
+	iva REAL NOT NULL,
+	total REAL NOT NULL,
+	modification_date_hour TIMESTAMP NOT NULL
+);
+
+CREATE TABLE purchase_details_log(
+	purchase_id INT NOT NULL,
+	material_id INT NOT NULL,
+	quantity INT NOT NULL,
+	unit_price REAL NOT NULL,
+	modification_date_hour TIMESTAMP NOT NULL
+);
+
+ALTER TABLE materials
+ADD CONSTRAINT fk_material_supplier FOREIGN KEY(supplier_id) REFERENCES suppliers (supplier_id);
+
+ALTER TABLE purchase_details
+ADD CONSTRAINT fk_purchase_details_material FOREIGN KEY(material_id) REFERENCES materials (material_id);
+
+ALTER TABLE purchase_details
+ADD CONSTRAINT fk_purchase_details_purchase FOREIGN KEY(purchase_id) REFERENCES purchase (purchase_id);
 
 ALTER TABLE purchase
 ADD CONSTRAINT fk_purchase_employees FOREIGN KEY(employee_id) REFERENCES employees(employee_id);
+----solo estan con fk las propias tablas las de historial solo se clonara pero sin dependecias----
 
-ALTER TABLE purchase_details
-ADD CONSTRAINT fk_purchase_details_purchase FOREIGN KEY(purchase_id) REFERENCES purchase(purchase_id);
+----Creacion de funciones disparadores----
+CREATE FUNCTION suppliers_log_funtion() RETURNS TRIGGER
+AS
+$$
+BEGIN
+	INSERT INTO suppliers_log VALUES(OLD.supplier_id,OLD.company_name,OLD.contact_name,OLD.contact_title,
+									OLD.address,OLD.email,
+									OLD.city, OLD.region,OLD.postal_code,
+									OLD.country,OLD.phone,OLD.fax,NOW());
+	RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
 
-ALTER TABLE purchase_details
-ADD CONSTRAINT fk_purchase_details_materials FOREIGN KEY(material_id) REFERENCES materials(material_id);
+CREATE FUNCTION materials_log_funtion() RETURNS TRIGGER
+AS
+$$
+BEGIN
+	INSERT INTO materials_log VALUES(OLD.material_id,OLD.supplier_id,OLD.material_name,
+									OLD.units_in_stock,OLD.material_measure,
+									OLD.unit_price, OLD.number_lot,OLD.expiration_date,
+									OLD.discontinued,NOW());
+	RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
 
-ALTER TABLE materials_old_date
-ADD CONSTRAINT fk_materials_old_date_materials FOREIGN KEY(material_id_old) REFERENCES materials(material_id);
+CREATE FUNCTION purchase_log_funtion() RETURNS TRIGGER
+AS
+$$
+BEGIN
+	INSERT INTO purchase_log VALUES(OLD.purchase_id,OLD.employee_id ,OLD.purchase_date,
+									OLD.purchase_status,OLD.observations,
+									OLD.delay, OLD.installment,OLD.days,
+									OLD.subtotal,OLD.iva,OLD.total,NOW());
+	RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
 
-ALTER TABLE materials_old_date
-ADD CONSTRAINT fk_materials_old_date_employees FOREIGN KEY(modification_employee) REFERENCES employees(employee_id);
+CREATE FUNCTION purchase_details_log_funtion() RETURNS TRIGGER
+AS
+$$
+BEGIN
+	INSERT INTO purchase_details_log VALUES(OLD.purchase_id,OLD.material_id ,OLD.quantity,
+									OLD.purchase_status,OLD.observations,
+									OLD.unit_price,NOW());
+	RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
 
-ALTER TABLE purchase_details_old_date
-ADD CONSTRAINT fk_purchase_details_old_date_materials FOREIGN KEY(material_id) REFERENCES materials(material_id);
+---Asignacion de disparadores---
+CREATE TRIGGER suppliers_log_update BEFORE UPDATE ON suppliers
+FOR EACH ROW
+EXECUTE PROCEDURE suppliers_log_funtion();
 
-ALTER TABLE purchase_details_old_date
-ADD CONSTRAINT fk_purchase_details_old_date_purchase FOREIGN KEY(purchase_id_old) REFERENCES purchase(purchase_id);
+CREATE TRIGGER materials_log_update BEFORE UPDATE ON materials
+FOR EACH ROW
+EXECUTE PROCEDURE materials_log_funtion();
 
-ALTER TABLE purchase_details_old_date
-ADD CONSTRAINT fk_purchase_details_old_date_employees FOREIGN KEY(modification_employee) REFERENCES employees(employee_id);
+CREATE TRIGGER purchase_log_update BEFORE UPDATE ON purchase
+FOR EACH ROW
+EXECUTE PROCEDURE purchase_log_funtion();
 
-ALTER TABLE purchase_old_date
-ADD CONSTRAINT fk_purchase_old_date_employees FOREIGN KEY(modification_employee) REFERENCES employees(employee_id);
-
-ALTER TABLE purchase_old_date
-ADD CONSTRAINT fk_purchase_old_date_purchase FOREIGN KEY(purchase_id_old_date) REFERENCES purchase(purchase_id);
+CREATE TRIGGER purchase_log_update BEFORE UPDATE ON purchase_details
+FOR EACH ROW
+EXECUTE PROCEDURE purchase_details_log_funtion();
